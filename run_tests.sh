1807c4dd2f3e920a8ff0050f35ccb976e7164384
Pierre Salvy <pierre.salvy@epfl.ch>
2018-10-03 17:23:30 +0200

    FIX: docker run test wrapped in a script

---
+++ run_tests.sh
@@ -0,0 +1,5 @@
+docker run --rm \
+        -v $(pwd)/work:/home/user/work \
+        -v $(pwd)/..:/openbread \
+        danielweilandt/openbread_docker 	\
+        bash -c "py.test --cov=./ /openbread/test"
