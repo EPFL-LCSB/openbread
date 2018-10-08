e4a1b98849bb99b2fb5319e5dd974aae938300c9
Pierre Salvy <pierre.salvy@epfl.ch>
2018-10-08 16:21:30 +0200

    FIX: Absolute paths in run_tests.sh

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 chmod -R u+X .
 docker run --rm \
-        -v $(pwd)/work:/home/user/work \
-        -v $(pwd)/..:/openbread \
+        -v $(pwd):/openbread \
+        -v $(pwd)/docker/work:/home/user/work \
         danielweilandt/openbread_docker 	\
         bash -c "py.test --cov=./ /openbread/test"
