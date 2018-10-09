280b33438d75588a5667efebabd5154e04f77223
Pierre Salvy <pierre.salvy@epfl.ch>
2018-10-09 13:33:07 +0200

    ENH/FIX: Travis does not support volumes in container calls, which is why everything failed. Fixing this with ad-hoc container in Dockerfile.ci

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 chmod -R u+X .
 docker run --rm \
-        -v $(pwd):/openbread \
-        -v $(pwd)/docker/work:/home/user/work \
-        danielweilandt/openbread_docker 	\
+        openbread_docker_ci 	\
         bash -c "py.test --cov=./ /openbread/test"
